0c3ad10ed871daba991a8ccbcd6841fab13d9c49
Nagrom <morgan.d.cox44@gmail.com>
2023-02-05 10:55:47 -0700

    got navbar working

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div className="app">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div
+      style={{
+        fontSize: "2rem",
+        display: "flex",
+        justifyContent: "center",
+        marginTop: "2rem",
+      }}
+    >
+      Home Page
+    </div>
+  );
+};
+
+export default HomePage;
